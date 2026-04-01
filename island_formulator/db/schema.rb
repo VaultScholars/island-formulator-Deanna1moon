b83217b7bbb7c198f3c451ec0bf76199ec24a495
Island Formulator Dev <dev@island-formulator.com>
2026-04-01 15:21:24 -0500

    final final

--- island_formulator/db/schema.rb
+++ island_formulator/db/schema.rb
@@ -133,8 +133,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_24_140537) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "email_address"
-    t.string "password_digest"
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
